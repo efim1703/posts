a2abb04d47a66da61e89f9be348d6878c8b33e1d
Dmitry Vlasov <17samara03@gmail.com>
2023-03-21 16:19:57 +0300

    feat: added store example

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: []
   },
   getters: {
+    getPosts(state) {
+      return state.posts
+    }
   },
   mutations: {
+    setPosts(state, mewPosts) {
+      state.posts = mewPosts
+    },
   },
   actions: {
+    initStore({ commit }, posts) {
+      commit('setPosts', posts)
+    },
   },
-  modules: {
-  }
 })
